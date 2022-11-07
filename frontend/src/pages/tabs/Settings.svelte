7898f1679cc4cd80dac502d955d85828fd0c2180
Luis Ojeda <leohub0103@gmail.com>
2022-11-07 18:27:23 -0500

    First change

--- frontend/src/pages/tabs/Settings.svelte
+++ frontend/src/pages/tabs/Settings.svelte
@@ -6,7 +6,7 @@
     <div class="container-fluid h-100">
         <div class="row h-100">
             <div class="col-12">
-                <p>SETTINGS</p>
+                <p>Profile</p>
             </div>
         </div>
     </div>
